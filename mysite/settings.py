9e3d84263a1bb43a62cb17bcd610ec77f88161c8
ishizawa <kar5bhkdkdwmf@icloud.com>
2020-10-28 13:51:30 +0900

    デプロイできない問題解決ファイル

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'ieor9-=$uf_bxu4=)q9gxlu#$*l_=cg5v(ompt%o6t9#zlm-u*'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'ippy.pythonanywhere.com']
 
 
 # Application definition
